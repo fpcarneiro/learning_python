3cf5654b2bb85a90fd3cbb9d5939342d0f1b410e
Fernando Carneiro <fpcar@yahoo.com.br>
2017-10-29 23:49:50 -0200

    #8 Commit

---
+++ HackerRank/Strings/alphabet_rangoli.py
@@ -0,0 +1,12 @@
+def print_rangoli(size):
+    alphabet = 'abcdefghijklimnoqrstuvwxyz'
+    c = "-".join(alphabet[size-1::-1] + alphabet[1:size])
+    for i in range(size-1, 0, -1):
+        front = alphabet[size-1:i:-1]
+        back = alphabet[i:size]
+        print("-".join(front + back).center(len(c), "-"))
+    print(c)
+    for i in range(1, size):
+        front = alphabet[size-1:i:-1]
+        back = alphabet[i:size]
+        print("-".join(front + back).center(len(c), "-"))
