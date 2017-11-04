337d3d882a69f455f3d299d6fd77f579a31f5a24
carneiro <fpcar@yahoo.com.br>
2017-11-03 22:14:37 -0200

    Commit #12

---
+++ HackerRank/Sets/introduction_to_sets.py
@@ -0,0 +1,11 @@
+def average(array):
+    # your code goes here
+    distinct = set(array)
+    return (sum(distinct)/len(distinct))
+
+
+if __name__ == "__main__":
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
