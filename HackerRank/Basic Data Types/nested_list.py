8572e4c5eaf3a2dace95ea471eef248aac841df9
carneiro <fpcar@yahoo.com.br>
2017-11-04 22:59:42 -0200

    Commit #15

--- HackerRank/Basic Data Types/nested_list.py
+++ HackerRank/Basic Data Types/nested_list.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
 
     grades = []
     [grades.append(g) for (s, g) in students if g not in grades]
-    [print(s) for (s, g) in students if g == grades[1]]
+    [s for (s, g) in students if g == grades[1]]
 
