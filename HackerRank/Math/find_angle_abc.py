65805255bd1969072f7cb98ff13f68de7e722ecb
Fernando Carneiro <fpcar@yahoo.com.br>
2017-11-08 14:59:02 -0200

    Update find_angle_abc.py

--- HackerRank/Math/find_angle_abc.py
+++ HackerRank/Math/find_angle_abc.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
+'''
+We can Solve this problem by using a property: ** That a median on the hypotenuse divides the right angled triangle in two isoceles triangle.** * Means AM=BM=CM * So, ∡MBC = ∡MCB
+'''
 import math
 
 AB = int(input())
 BC = int(input())
-AC = math.sqrt(AB**2 + BC**2)
-MC = AC/2
+#AC = math.sqrt(AB**2 + BC**2)
+#MC = AC/2
 symbol = '°'
-d = symbol.encode('utf8')
-print(symbol)
-#print(str(d, encoding='utf8'))
-#print(str(round(math.degrees(math.asin(MC/BC)))) + u'\u00b0')
+#d = symbol.encode('utf8')
+#print(symbol)
+print(str(round(math.degrees(math.atan(AB/BC)))) + symbol)
