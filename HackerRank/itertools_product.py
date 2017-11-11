76588e7a2ddcf108731bf1cd462fd484219de15a
carneiro <fpcar@yahoo.com.br>
2017-11-10 22:13:30 -0200

    Commit #22

--- HackerRank/itertools_product.py
+++
@@ -1,9 +0,0 @@
-from itertools import product
-A = [int(e) for e in input().split(" ")]
-B = list(map(int, input().split(" ")))
-A.sort()
-B.sort()
-C = list(product(A, B))
-C.sort()
-for e in C:
-    print(e, end=" ")
