09f50c2b28d75fd37a5b31385345e0fb70d5ea9c
carneiro <fpcar@yahoo.com.br>
2017-11-10 22:27:17 -0200

    Commit #25

--- HackerRank/Math/triangle_quest.py
+++ HackerRank/Math/triangle_quest.py
@@ -1,2 +1,2 @@
-for i in range(1, int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+for i in range(1, int(input()) + 1):
     print(sum(map((lambda x, y: x*y), (map((lambda x: 10**x), (range(i-1, -1, -1)))), ([i] * i))))
