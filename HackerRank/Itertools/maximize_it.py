50c3257008c78dbd93a990e4f3525b6be4ba5f39
carneiro <fpcar@yahoo.com.br>
2017-11-12 00:31:13 -0200

    Commit #32

--- HackerRank/Itertools/maximize_it.py
+++ HackerRank/Itertools/maximize_it.py
@@ -1,7 +1,6 @@
 from itertools import product
 
-values = [int(v) for v in input().split()]
-k, m = values[0], values[1]
+[k, m] = [int(v) for v in input().split()]
 lists_squared = [list(map((lambda x: x ** 2), map(int, (input().split()[1:])))) for i in range(k)]
 l = list(product(*lists_squared))
 print(max(list(map(lambda x: sum(x) % m, l))))
