e25c8c40ab1405d306690ace6b3ce84a24ba36c9
Fernando Carneiro <fpcar@yahoo.com.br>
2018-01-28 11:42:18 -0200

    # Commit #DefaultDict

--- HackerRank/Python/Collections/defaultdict_tutorial.py
+++ HackerRank/Python/Collections/defaultdict_tutorial.py
@@ -1,14 +1,10 @@
 from collections import defaultdict
 A = defaultdict(list)
-B = defaultdict(list)
 
 m, n = list(map(int, input().split()))
 for i in range(m):
     A[input()].append(i+1)
-for i in range(n):
-    B[input()].append(i + 1)
+B = [input() for i in range(n)]
 
-for e in B.keys():
-    for el in A.get(e, [-1]):
-        print(el, end=" ")
-    print()
+for e in B:
+    print(" ".join(map(str, A.get(e, [-1]))))
