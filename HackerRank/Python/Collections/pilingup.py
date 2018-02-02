aba202409e4ffca654cf7b5a09b0679cc0174de9
Fernando Carneiro <fpcar@yahoo.com.br>
2018-02-02 18:34:37 -0200

    Update pilingup.py

--- HackerRank/Python/Collections/pilingup.py
+++ HackerRank/Python/Collections/pilingup.py
@@ -1,20 +1,31 @@
 from collections import deque
+import re
 
 def sayit(seq):
-    response = "Yes"
-    while len(seq) and response == "Yes":
-        max_pos = seq.index(max(seq))
-        if max_pos == 0:
-            seq.popleft()
-        elif max_pos == len(seq)-1:
-            seq.pop()
-        else:
-            response = "No"
-    return response
+    response = True
+    while (len(seq) >= 1) and response:
+        max, seq, response = sayit2(seq)
+        print(seq)
+    return (len(seq) <= 1)
+    
+def sayit2(seq):
+    d = deque(map(int, seq.split()))
+    max_number = max(d)
+    pattern_ini = r'^(' + str(max_number) + '\s)' + '+'
+    pattern_end = r'(\s' + str(max_number) + ')' + '+' + '$'
+    p_ini = re.compile(pattern_ini)
+    p_end = re.compile(pattern_end)
+        
+    seq, i = p_ini.subn( '', seq)
+    seq, e = p_end.subn( '', seq)
+    
+    response = ( i + e) != 0
+    
+    return max_number, seq, response
 
 if __name__ == "__main__":
     for tc in range(int(input())):
         s = int(input())
-        row_of_cubes = deque(map(int, input().split()))
-        response = sayit(row_of_cubes)
+        row_of_cubes = input()
+        response = sayit2(row_of_cubes)
         print(response)
